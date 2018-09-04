437071eb1af8cb4883898a1c154b1765812ebd7b
Kevin Simpson <relasine@gmail.com>
2018-09-04 14:53:33 -0600

    Link current weather to app.js

---
+++ src/current/Current.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './current.css';
+
+function Current() {
+	return (
+		<div className="Current">
+			<h1>Hai frens</h1>
+		</div>
+		)
+}
+
+export default Current;
