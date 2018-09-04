7b2cdfd1a7bade2445c20cbce1a3ff3157a27b9c
Kevin Simpson <relasine@gmail.com>
2018-09-04 17:22:53 -0600

    Add current.js layout and some styling

--- src/current/Current.js
+++ src/current/Current.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './current.css';
+// import weatherPic from './'
 
 function Current() {
+
 	return (
-		<div className="Current">
-			<h1>Hai frens</h1>
+		<div className="current">
+			<aside className="weather-img-aside"> 
+				<img src="/partly-cloudy.svg" alt="partlycloudy" className="weather-img"/>
+			</aside>
+			<article>
+				<h1 className="temperature">72°</h1>
+				<h2 className="wind"><span className="weather-label">Wind - </span> NNE 5mph</h2>
+				<h2 className="humidity"><span className="weather-label">Humidity - </span> 24%</h2>
+				<h2 className="visibility"><span className="weather-label">Visibility - </span> 10.0 mi</h2>
+				<h2 className="weather-description">Partly cloudy</h2>
+
+			</article>
 		</div>
-		)
+	)
 }
 
 export default Current;
