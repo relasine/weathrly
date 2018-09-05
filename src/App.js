4edfadb7c8ab1caf9e3d04802559e898de0a9e26
Cierra Washington <cierrajwashi@gmail.com>
2018-09-05 16:01:39 -0600

    Add mock data to Current page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Current from './current/Current'
+import { data } from './api.js'
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      data: data
+    }
+  }
+
+
   render() {
+
+    console.log({data});
     return (
       <div className="App">
-        <Current />
+        <Current data={ this.state.data }/>
       </div>
     );
   }
--- src/current/Current.js
+++ src/current/Current.js
@@ -2,19 +2,22 @@ import React from 'react';
 import './current.css';
 // import weatherPic from './'
 
-function Current() {
+function Current(props) {
+
+console.log(props.data.current_observation);
 
 	return (
 		<div className="current">
-			<aside className="weather-img-aside"> 
+			<aside className="weather-img-aside">
 				<img src="/partly-cloudy.svg" alt="partlycloudy" className="weather-img"/>
-				<h2 className="weather-description">Partly cloudy</h2>
+				<h2 className="weather-description">{props.data.current_observation.weather}</h2>
 			</aside>
 			<article>
-				<h1 className="temperature">72°</h1>
-				<h2 className="wind"><span className="weather-label wind-data">Wind - </span> NNE 5 mph</h2>
-				<h2 className="humidity"><span className="weather-label humidity-data">Humidity - </span> 24%</h2>
-				<h2 className="visibility"><span className="weather-label visibility-data">Visibility - </span> 10.0 mi</h2>
+
+				<h1 className="temperature">{props.data.current_observation.temp_f}°</h1>
+				<h2 className="wind"><span className="weather-label wind-data">Wind - </span>{props.data.current_observation.wind_dir} {props.data.current_observation.wind_mph} mph</h2>
+				<h2 className="humidity"><span className="weather-label humidity-data">Humidity - </span> {props.data.current_observation.relative_humidity}</h2>
+				<h2 className="visibility"><span className="weather-label visibility-data">Visibility - </span> {props.data.current_observation.visibility_mi} mi</h2>
 			</article>
 		</div>
 	)
