553825f3380f447ca1f0155b498d4b58936ac94a
Kevin Simpson <relasine@gmail.com>
2018-09-04 20:32:37 -0600

    Add styling and first media query for phase one

--- src/current/Current.js
+++ src/current/Current.js
@@ -8,14 +8,13 @@ function Current() {
 		<div className="current">
 			<aside className="weather-img-aside"> 
 				<img src="/partly-cloudy.svg" alt="partlycloudy" className="weather-img"/>
+				<h2 className="weather-description">Partly cloudy</h2>
 			</aside>
 			<article>
 				<h1 className="temperature">72°</h1>
-				<h2 className="wind"><span className="weather-label">Wind - </span> NNE 5mph</h2>
+				<h2 className="wind"><span className="weather-label">Wind - </span> NNE 5 mph</h2>
 				<h2 className="humidity"><span className="weather-label">Humidity - </span> 24%</h2>
 				<h2 className="visibility"><span className="weather-label">Visibility - </span> 10.0 mi</h2>
-				<h2 className="weather-description">Partly cloudy</h2>
-
 			</article>
 		</div>
 	)
