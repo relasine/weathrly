104c035dd1e86db084b6c0ece0606287a4c08c99
Cierra Washington <cierrajwashi@gmail.com>
2018-09-08 11:20:23 -0600

    Add box sizing to index file and align input with magnifying glass for landing page

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './welcome.css'
+
 export default class Welcome extends Component{
   constructor(){
     super();
@@ -12,9 +14,9 @@ export default class Welcome extends Component{
   render(){
 
     return(
-      <form>
-      <input type='text' name="location-input" placeholder='City/Zip'></input>
-      <img src="./magnifier.svg"/>
+      <form className="location-container">
+      <input className="location-input" type='text' name="location-input" placeholder='City/Zip'></input>
+      <div className="magnifier-div"><img className="magnifier" src="./magnifier.svg"/></div>
       </form>
 
     )
