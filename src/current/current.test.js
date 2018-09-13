1f72b4c5292a4ea866da8ac26a6da1473e94ff55
Cierra Washington <cierrajwashi@gmail.com>
2018-09-12 19:31:12 -0600

    Write tests for CityOption, Current, Daily

--- src/current/current.test.js
+++ src/current/current.test.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import Current from './Current'
+import Current from './Current';
 import { mount, shallow } from 'enzyme';
+import { data } from '../fakeapi';
 
 describe('Current', ()=>{
 
   let wrapper;
 
-  // beforeEach(()=>{
-  //   wrapper = shallow(<Current data='display_location.full' />)
-  // });
+  let props = {
+    data: data.current_observation,
+    forecastTemp: data.forecast.simpleforecast.forecastday[0]
+  }
 
-  it('should exist', () => {
-    // expect(wrapper).toBeDefined();
+  beforeEach(()=>{
+    wrapper = shallow(<Current data={props.data} forecastTemp={props.forecastTemp}/>)
   });
 
-  // it('should render content to the DOM', () => {
-  //   expect(wrapper.find('div').length).toBe(1);
-  //   expect(wrapper.find('section').length).toBe(1);
-  //   expect(wrapper.find('aside').length).toBe(1);
-  //   expect(wrapper.find('article').length).toBe(1);
-  //   expect(wrapper.find('h1').length).toBe(1);
-  //   expect(wrapper.find('h2').length).toBe(3);
-  // });
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  });
 
-  // it('should have props', () => {
-  //   expect(wrapper.props().data).toEqual('display_location')
-  // })
+  it('should render content to the DOM', () => {
+    expect(wrapper.find('div').length).toBe(1);
+    expect(wrapper.find('section').length).toBe(1);
+    expect(wrapper.find('aside').length).toBe(1);
+    expect(wrapper.find('article').length).toBe(1);
+    expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper.find('h2').length).toBe(4);
+  });
+
+  it('should have props', () => {
+
+    expect(wrapper.props().children[0].props.children[0]).toEqual('Louisville, KY')
+  })
+
+
+
 })
